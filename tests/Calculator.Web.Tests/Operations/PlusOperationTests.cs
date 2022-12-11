c1832070f1c631d5914d5d69cee32c0038b4bdf6
YauheniKorsakas <yauheni.korsakas@gmail.com>
2022-12-11 17:57:03 +0300

    Add tests and udpate solution

---
+++ tests/Calculator.Web.Tests/Operations/PlusOperationTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Web.Tests.Operations
+{
+    [TestFixture]
+    public class PlusOperationTests
+    {
+
+
+    }
+}
